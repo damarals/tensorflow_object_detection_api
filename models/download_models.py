eb1598f380d82ed1bcd206cc6a2088f052bd6c69
Daniel Amaral <silva.daniel86@Gmail.com>
2021-12-05 14:13:21 -0300

    :sparkles: eficientdet-d4 to centernet

--- models/download_models.py
+++ models/download_models.py
@@ -12,12 +12,12 @@ makedirs(dirname(options.models_dir + '/'), exist_ok = True)
 
 models_url = ['http://download.tensorflow.org/models/object_detection/tf2/20200713/centernet_hg104_512x512_coco17_tpu-8.tar.gz',
  'http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d0_coco17_tpu-32.tar.gz',
- 'http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d4_coco17_tpu-32.tar.gz']
+ 'http://download.tensorflow.org/models/object_detection/tf2/20200713/centernet_hg104_1024x1024_coco17_tpu-32.tar.gz']
 models_name = [m.rsplit('/', 1)[1] for m in models_url]
 
 configs_url = ['https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/configs/tf2/centernet_hourglass104_512x512_coco17_tpu-8.config',
  'https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/configs/tf2/ssd_efficientdet_d0_512x512_coco17_tpu-8.config',
- 'https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/configs/tf2/ssd_efficientdet_d4_1024x1024_coco17_tpu-32.config']
+ 'https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/configs/tf2/centernet_hourglass104_1024x1024_coco17_tpu-32.config']
 configs_name = [c.rsplit('/', 1)[1] for c in configs_url]
 
 
