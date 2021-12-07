f1e11554435516998968fc35a5dab8f01fdb957c
Daniel Amaral <silva.daniel86@Gmail.com>
2021-12-07 19:38:21 -0300

    :bug: fix models to model

--- models/download_models.py
+++ models/download_models.py
@@ -10,13 +10,13 @@ p.add_option('--download_model_url')
 p.add_option('--download_config_url')
 options, arguments = p.parse_args()
 
-makedirs(dirname(options.models_dir + '/'), exist_ok = True)
+makedirs(dirname(options.model_dir + '/'), exist_ok = True)
 
 model_name = options.download_model_url.rsplit('/', 1)[1]
 config_name = options.download_config_url.rsplit('/', 1)[1]
 
 if options.download_config_url != 'default':
-	wget.download(options.download_config_url, options.models_dir + '/' + config_name)
+	wget.download(options.download_config_url, options.model_dir + '/' + config_name)
 
 wget.download(options.download_model_url, options.model_dir + '/' + model_name)
-unpack_archive(options.models_dir + '/' + model_name, options.models_dir + '/')
+unpack_archive(options.model_dir + '/' + model_name, options.model_dir + '/')
