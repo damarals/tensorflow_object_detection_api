35873d06ff8fe14a8800c0a05110369fa8ed6b74
Daniel Amaral <silva.daniel86@Gmail.com>
2021-12-07 19:18:41 -0300

    :sparkles: add support to bash variables

--- models/download_models.py
+++ models/download_models.py
@@ -5,25 +5,18 @@ import wget
 from shutil import unpack_archive
 
 p = OptionParser()
-p.add_option('--models_dir')
+p.add_option('--model_dir')
+p.add_option('--download_model_url')
+p.add_option('--download_config_url')
 options, arguments = p.parse_args()
 
 makedirs(dirname(options.models_dir + '/'), exist_ok = True)
 
-models_url = ['http://download.tensorflow.org/models/object_detection/tf2/20200713/centernet_hg104_512x512_coco17_tpu-8.tar.gz',
- 'http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d0_coco17_tpu-32.tar.gz',
- 'http://download.tensorflow.org/models/object_detection/tf2/20200713/centernet_hg104_1024x1024_coco17_tpu-32.tar.gz']
-models_name = [m.rsplit('/', 1)[1] for m in models_url]
+model_name = options.download_model_url.rsplit('/', 1)[1]
+config_name = options.download_config_url.rsplit('/', 1)[1]
 
-configs_url = ['https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/configs/tf2/centernet_hourglass104_512x512_coco17_tpu-8.config',
- 'https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/configs/tf2/ssd_efficientdet_d0_512x512_coco17_tpu-8.config',
- 'https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/configs/tf2/centernet_hourglass104_1024x1024_coco17_tpu-32.config']
-configs_name = [c.rsplit('/', 1)[1] for c in configs_url]
+if options.download_config_url != 'default':
+	wget.download(options.download_config_url, options.models_dir + '/' + config_name)
 
-
-for url, model in zip(models_url, models_name):
-  wget.download(url, options.models_dir + '/' + model)
-  unpack_archive(options.models_dir + '/' + model, options.models_dir + '/')
-
-for url, config in zip(configs_url, configs_name):
-  wget.download(url, options.models_dir + '/' + config)
+wget.download(options.download_model_url, options.model_dir + '/' + model_name)
+unpack_archive(options.models_dir + '/' + model_name, options.models_dir + '/')
