4f37a0558651b1492df8ca072d54cb8817c2d949
Daniel Amaral <silva.daniel86@Gmail.com>
2021-12-07 20:36:56 -0300

    :bug: fix model config default

--- models/download_models.py
+++ models/download_models.py
@@ -15,7 +15,7 @@ makedirs(dirname(options.model_dir + '/'), exist_ok = True)
 model_name = options.download_model_url.rsplit('/', 1)[1]
 config_name = options.download_config_url.rsplit('/', 1)[1]
 
-if options.download_config_url != 'default':
+if config_name != 'default':
 	wget.download(options.download_config_url, options.model_dir + '/' + config_name)
 
 wget.download(options.download_model_url, options.model_dir + '/' + model_name)
