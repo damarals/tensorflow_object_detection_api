cc030cdae81ba7564c232538f74bc41c8cb40a70
Daniel Amaral <silva.daniel86@Gmail.com>
2021-12-07 20:12:49 -0300

    :sparkles: add support to bash variables and define default model pipeline

--- models/edit_config.py
+++ models/edit_config.py
@@ -4,17 +4,14 @@ from optparse import OptionParser
 
 p = OptionParser()
 p.add_option('--models_dir')
+p.add_option('--download_model_url')
+p.add_option('--download_config_url')
 options, arguments = p.parse_args()
 
 path = options.models_dir + '/'
 
-config_list = ['centernet_hourglass104_512x512_coco17_tpu-8.config',
-               'ssd_efficientdet_d0_512x512_coco17_tpu-8.config',
-               'centernet_hourglass104_1024x1024_coco17_tpu-32.config']
-models_folder = ['centernet_hg104_512x512_coco17_tpu-8',
-                 'efficientdet_d0_coco17_tpu-32',
-                 'centernet_hg104_1024x1024_coco17_tpu-32']
-models_folder = [c.rsplit('.tar', 1)[0] for c in models_folder]
+config_name = options.download_config_url.rsplit('/', 1)[1] 
+model_folder = options.download_model_url.rsplit('/', 1)[1].rsplit('.tar', 1)[0] 
 
 labelmap_path = "/content/dissertacao/data/images/labelmap.pbtxt"
 train_record_path = "/content/dissertacao/data/images/train.record"
@@ -23,44 +20,47 @@ num_classes = 1
 batch_size = 8
 num_steps = 8000
 
-for config_file, config_folder in zip(config_list, models_folder):
-  fine_tune_checkpoint = "/content/dissertacao/models/pretrained/" + config_folder + "/checkpoint/ckpt-0"
+fine_tune_checkpoint = "/content/dissertacao/models/pretrained/" + config_folder + "/checkpoint/ckpt-0"
 
-  with open(path + config_file) as f:
-    config = f.read()
+if config_name == 'default':
+    with open(path + model_folder + '/pipeline.config') as f:
+        config = f.read()
+else:
+    with open(path + config_name) as f:
+        config = f.read()
 
-  with open(path + config_folder + '/pipeline_file.config', 'w') as f:
-    
+with open(path + config_folder + '/pipeline_file.config', 'w') as f:
+
     # Set labelmap path
     config = re.sub('label_map_path: ".*?"', 
                     'label_map_path: "{}"'.format(labelmap_path), config)
-    
+
     # Set fine_tune_checkpoint path
     config = re.sub('fine_tune_checkpoint: ".*?"',
                     'fine_tune_checkpoint: "{}"'.format(fine_tune_checkpoint), config)
-    
+
     # Set train tf-record file path
     config = re.sub('(input_path: ".*?)(PATH_TO_BE_CONFIGURED/train)(.*?")', 
                     'input_path: "{}"'.format(train_record_path), config)
-    
+
     # Set test tf-record file path
     config = re.sub('(input_path: ".*?)(PATH_TO_BE_CONFIGURED/val)(.*?")', 
                     'input_path: "{}"'.format(test_record_path), config)
-    
+
     # Set number of classes.
     config = re.sub('num_classes: [0-9]+',
                     'num_classes: {}'.format(num_classes), config)
-    
+
     # Set batch size
     config = re.sub('batch_size: [0-9]+',
                     'batch_size: {}'.format(batch_size), config)
-    
+
     # Set training steps
     config = re.sub('num_steps: [0-9]+',
                     'num_steps: {}'.format(num_steps), config)
-    
+
     # Set fine-tune checkpoint type to detection
     config = re.sub('fine_tune_checkpoint_type: "classification"', 
                     'fine_tune_checkpoint_type: "{}"'.format('detection'), config)
-    
+
     f.write(config)
